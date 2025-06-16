fba6c4afe05799a38919048b41088445cb27f7fc
Rafae1TCC <rafael.cabrera32@uabc.edu.mx>
2025-06-16 13:35:35 -0700

    Final Report

--- issues/views.py
+++ issues/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import (ListView, DetailView, CreateView, UpdateView, DeleteView)
 from .models import Issue
+from .models import Status
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 
@@ -10,34 +11,37 @@ class IssueListView(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
-        todo = Status.objects.get(name="to-do")
+        todo_status = Status.objects.get(name="to_do")
+        progress_status = Status.objects.get(name="in_progress")
+        done_status = Status.objects.get(name="done")
 
-        context["to-do-list"] = (
-            Post.objects
-            .filter(status=published)
-            .order_by('created_on').reverse()
+        context["to_do_list"] = (
+            Issue.objects
+            .filter(status=todo_status)
+            .order_by('-created_on')
         )
 
-        progress = Status.objects.get(name="in-progress")
-
-        context["in-progress-list"] = (
-            Post.objects
-            .filter(status=published)
-            .order_by('created_on').reverse()
+        context["in_progress_list"] = (
+            Issue.objects
+            .filter(status=progress_status)
+            .order_by('-created_on')
         )
 
-        done = Status.objects.get(name="done")
-
-        context["done-list"] = (
-            Post.objects
-            .filter(status=published)
-            .order_by('created_on').reverse()
+        context["done_list"] = (
+            Issue.objects
+            .filter(status=done_status)
+            .order_by('-created_on')
         )
         return context
 
-class IssueDetailView(DetailView):
+class IssueDetailView(DetailView, LoginRequiredMixin, UpdateView):
     template_name = 'issues/detail.html'
     model = Issue
+    fields = ["status"]
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 class IssueCreateView(LoginRequiredMixin, CreateView):
     template_name = 'issues/new.html'
